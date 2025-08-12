22a878989bc55e90841a53cfe7a46aa06230bd83
chicken-road-new <olehator_mb@044.agency>
2025-08-12 14:25:47 +0300

    Add files via upload

---
+++ samen/thanks.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+	<title>Bedankt</title>
+	<meta charset="UTF-8">
+	<meta name="keywords" content="#" />
+	<meta name="format-detection" content="telephone=no">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link
+		href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,300;0,400;0,500;0,700;0,900;1,400&display=swap&_v=20231205133949"
+		rel="stylesheet">
+	<link rel="stylesheet" href="css/style.min.css">
+	<link rel="shortcut icon" href="favicon.ico">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body class="black">
+
+	<style>
+		/* Стилі для сторінки "Bedankt" */
+		.access-denied {
+			text-align: center;
+			padding: 50px;
+			font-family: 'Inter', sans-serif;
+			background-color: #f8f9fa;
+			min-height: 100vh;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+		}
+
+		.access-denied__title {
+			font-size: 36px;
+			color: #333;
+			margin-bottom: 10px;
+			font-weight: bold;
+		}
+
+		.access-denied__sub-title {
+			font-size: 18px;
+			color: #555;
+		}
+	</style>
+	<div class="wrapper">
+		<section class="access-denied">
+			<div class="access-denied__container">
+				<h1 class="access-denied__title">
+					Bedankt voor je aanmelding!
+				</h1>
+				<h2 class="access-denied__sub-title">
+					We nemen binnenkort contact met je op!
+				</h2>
+			</div>
+		</section>
+	</div>
+</body>
+
+</html>
